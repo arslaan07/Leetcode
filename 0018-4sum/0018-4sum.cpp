510996636b8f1508f882826faf0b1d1a31f41c5f
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-10-06 10:46:40 +0530

    Time: 64 ms (52.79%), Space: 13.5 MB (53.32%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -5,11 +5,11 @@ public:
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++){
-            if(i!=0  && nums[i]==nums[i-1]) {
+            if(i>0  && nums[i]==nums[i-1]) {
                 continue;
             }
             for(int j=i+1;j<n;j++){
-            if(j!=i+1 && nums[j]==nums[j-1]) {
+            if(j>i+1 && nums[j]==nums[j-1]) {
                 continue;
             }
                 int k=j+1;
