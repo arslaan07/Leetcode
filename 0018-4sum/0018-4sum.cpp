33f640df9b0d5b326ab14a79ab5df4d4e11848ab
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-10-06 10:57:30 +0530

    Time: 68 ms (42.20%), Space: 13.7 MB (36.41%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,45 +1,47 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            if(i>0  && nums[i]==nums[i-1]) {
+    vector<vector<int>> fourSum(vector<int>& a, int target) {
+    int n=a.size();
+    sort(a.begin(),a.end());
+    vector<vector<int>> ans;
+    for(int i=0;i<n;i++){
+        if(i>0 && a[i]==a[i-1]){
+            continue;
+        }
+        for(int j=i+1;j<n;j++){
+            if(j>i+1 && a[j]==a[j-1]){
                 continue;
             }
-            for(int j=i+1;j<n;j++){
-            if(j>i+1 && nums[j]==nums[j-1]) {
-                continue;
-            }
-                int k=j+1;
-                int l=n-1;
-                while(k<l){
-                    long long sum=nums[i];
-                    sum+=nums[j];
-                    sum+=nums[k];
-                    sum+=nums[l];
-                    if(sum==target){
-                        vector<int>temp={nums[i],nums[j],nums[k],nums[l]};
-                        ans.push_back(temp);
-                        k++;
-                        l--;
-                        while(k<l&& nums[l]==nums[l+1]){
-                            l--;
-                        }
-                        while(k<l&& nums[k]==nums[k-1]){
-                            k++;
-                        }
-                        
-                    }
-                    else if(sum<target){
-                        k++;
-                    }else{
-                        l--;
-                    }
+            int k=j+1;
+            int l=n-1;
+            while(k<l){
+               
+                long long sum=a[i];
+                sum+=a[j];
+                sum+=a[k];
+                sum+=a[l];
+                if(sum==target){
+                    vector<int> temp={a[i],a[j],a[k],a[l]};
+                    ans.push_back(temp);
+                     k++;
+                    l--;
+                while(k<l && a[l]==a[l+1]){
+                    l--;
                 }
+                while(k<l && a[k]==a[k-1]){
+                    k++;
+                }
+                }
+                else if(sum>target){
+                    l--;
+                }
+                else{
+                    k++;
+                }
+                
             }
         }
-        return ans;
+    }
+    return ans;
     }
 };
