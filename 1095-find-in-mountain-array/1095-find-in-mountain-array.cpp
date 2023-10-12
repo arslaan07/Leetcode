9edbee07298b3117c3cbd2f78f27c99b3b8e4671
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-10-12 17:25:40 +0530

    Time: 3 ms (78.78%), Space: 7.4 MB (27.99%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -0,0 +1,71 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
+class Solution {
+public:
+    int findPeakIndex(int target, MountainArray &arr,int n){
+        int low=0;
+        int high=n-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(arr.get(mid)<arr.get(mid+1)){
+                low=mid+1;
+            }
+            else {
+                high=mid;
+            }
+        }
+        return low;
+        
+    } 
+    int searchLeftMountain(int target, MountainArray &arr,int peakIndex){
+        int low=0;
+        int high=peakIndex;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr.get(mid)==target){
+                return mid;
+            }
+            else if(arr.get(mid)>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+    int searchRighMountain(int target, MountainArray &arr,int peakIndex,int n){
+        int low=peakIndex+1;
+        int high=n-1;
+         while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr.get(mid)==target){
+                return mid;
+            }
+            else if(arr.get(mid)>target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return -1;
+        
+    }
+    int findInMountainArray(int target, MountainArray &arr) {
+        int n=arr.length();
+        int peakIndex=findPeakIndex(target,arr,n);
+        int left=searchLeftMountain(target,arr,peakIndex);
+        if(left!=-1) return left;
+        int right=searchRighMountain(target,arr,peakIndex,n);
+        return right;
+    }
+};
