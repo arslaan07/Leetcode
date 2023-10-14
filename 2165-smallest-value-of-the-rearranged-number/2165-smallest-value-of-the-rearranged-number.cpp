af30f5bfd60eb52f3e5f5fc452ea860530c6961d
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-10-14 21:11:21 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (15.87%) - LeetHub

--- 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
+++ 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
@@ -2,30 +2,23 @@ class Solution {
 public:
     long long smallestNumber(long long num) {
     if(num==0) return 0;
-    long long temp=abs(num);
-    vector<long long> v;
-    while(temp){
-       v.push_back(temp%10);
-       temp=temp/10;
-    }
+    string s=to_string(num);
     if(num>0)
-    sort(v.begin(),v.end());
+    sort(s.begin(), s.end());
     else
-    sort(v.begin(),v.end(),greater<int>());
-    int j;
-    if(v[0]==0){
-    for(j=1;j<v.size();j++){
-        if(v[j]!=0){
-            swap(v[j],v[0]);
+    sort(s.begin(), s.end(),greater<int>());
+    
+    if(s[0]=='0'){
+        int j;
+    for(j=1;j<s.size();j++){
+        if(s[j]!='0'){
+            swap(s[j],s[0]);
             break;
         }
     }   
     }
-    long long result=0;
-    for(int i=0;i<v.size();i++){
-        result=result*10+v[i];
-    }
+    long long result=stoll(s);
     
-    return num<0? -result : result;;
+    return num<0? -1LL*result : result;
     }
 };
