33a850949aa4693359193d83b8ad8bc9a2c47140
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-10-14 20:58:41 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (15.87%) - LeetHub

--- 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
+++ 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
@@ -1,25 +1,5 @@
 class Solution {
 public:
-    static bool pComparator(int a,int b){
-    string s_a=to_string(a);
-    string s_b=to_string(b);
-    string s_a_b=s_a + s_b;
-    string s_b_a=s_b + s_a;
-    if(s_a_b < s_b_a){
-        return true;
-    }
-    return false;
-}
-    static bool nComparator(int a,int b){
-    string s_a=to_string(a);
-    string s_b=to_string(b);
-    string s_a_b=s_a + s_b;
-    string s_b_a=s_b + s_a;
-    if(s_a_b > s_b_a){
-        return true;
-    }
-    return false;
-}
     long long smallestNumber(long long num) {
     if(num==0) return 0;
     long long temp=abs(num);
@@ -28,10 +8,10 @@ public:
        v.push_back(temp%10);
        temp=temp/10;
     }
-    if(num>=0)
-    sort(v.begin(),v.end(),pComparator);
+    if(num>0)
+    sort(v.begin(),v.end());
     else
-    sort(v.begin(),v.end(),nComparator);
+    sort(v.begin(),v.end(),greater<int>());
     int j;
     if(v[0]==0){
     for(j=1;j<v.size();j++){
