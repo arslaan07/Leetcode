f7582f348196afe125e5dd0db662d3e5dfbc1d83
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-10-15 20:50:59 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (35.32%) - LeetHub

---
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int largestInteger(int num) {
+        string str=to_string(num);
+        string even;
+        string odd;
+       
+        for(int i=0;i<str.size();i++){
+            if((str[i]-'0')%2==1){
+                odd.push_back(str[i]);
+                
+            }
+            else{
+                even.push_back(str[i]);
+            }
+        }
+        sort(odd.begin(),odd.end(),greater<int>());
+        sort(even.begin(),even.end(),greater<int>());
+        string ans="";
+        int j=0;
+        int k=0;
+        for(int i=0;i<str.size();i++){
+            if((str[i]-'0')%2==1)
+                ans[i]=odd[j++];
+                   
+            else
+                ans[i]=even[k++];
+                
+            
+        }
+        
+        
+        
+        int result=stoi(ans);
+        return result;
+        
+    }
+};
