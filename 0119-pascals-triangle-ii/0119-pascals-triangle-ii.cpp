623eef962fb1509c6bf83e154d58258d2e797b10
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-10-16 18:49:21 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (17.31%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,13 +1,5 @@
 class Solution {
 public:
-    // void factorial(int n,int r,vector<int>& ds){
-    // long long ans=1;
-    // for(int i=1;i<=r;i++){
-    //     ans=ans*(n-i+1);
-    //     ans=ans/i;
-    // }
-    //     ds.push_back(ans);
-    // }
     vector<int> getRow(int rowIndex) {
         vector<int> ds;
         long long ans=1;
