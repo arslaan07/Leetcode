3839390e32f4f7105fcc16b0574f2c0891d483f4
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-10-19 18:51:00 +0530

    Time: 87 ms (66.97%), Space: 26.2 MB (63.25%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string answer = "";
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '*')
+                answer.pop_back();
+            else
+                answer.push_back(s[i]);
+        }
+        return answer;
+    }
+};
