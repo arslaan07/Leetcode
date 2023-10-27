f276e6d5337edf80f33ad289e54ee17cd571a812
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-10-27 11:20:36 +0530

    Time: 23 ms (64.93%), Space: 20.5 MB (45.23%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,34 +1,45 @@
 class Solution {
 public:
-    string longestPalindrome(string str) {
-    int n=str.length();
-    int length=0;
-    int maxLength=0;
-    string longString="";
-    for (int i = 0; i < n; i++) {
-        // Counting odd-length palindromes
-        int j=0;
-        while(i-j>=0 && i+j<=n-1 && str[i-j]==str[i+j]){
-            length=2*j+1;
-            if(length>maxLength){
-                maxLength=length;
-                longString=str.substr(i-j,2*j+1);
+    string oddLengthPalindrome(string s){
+        int n=s.size();
+        string longestString="";
+        int maxCount=0; 
+        for(int i=0;s[i]!='\0';i++){
+            int j=0;
+            int count=0;
+            while(i-j>=0 && i+j<=n-1 && s[i-j]==s[i+j]){
+                count++;
+                if(count>maxCount){
+                    maxCount=count;
+                    longestString=s.substr(i-j,2*j+1);
+                }
+                j++;     
             }
-            j++;
         }
+        return longestString;
     }
-    for (double i = 0.5; i < n; i++) {
-        // Counting even-length palindromes
-        double j=0.5;
-        while(i-j>=0 && i+j<=n-1 && str[int(i-j)]==str[int(i+j)]){
-            length=2*j+1;
-             if(length>maxLength){
-                maxLength=length;
-                longString=str.substr(i-j,2*j+1);
+        string evenLengthPalindrome(string s){
+        int n=s.size();
+        int maxCount=0;
+        string longestString="";
+        for(double i=0.5;s[i]!='\0';i++){
+            double j=0.5;
+            int count=0;
+            while(i-j>=0 && i+j<=n-1 && s[i-j]==s[i+j]){
+                count++;
+                if(count>maxCount){
+                    maxCount=count;
+                    longestString=s.substr(i-j,2*j+1);
+                }
+                j++;     
             }
-            j++;
         }
+            return longestString;
+        
     }
-    return longString;
+    string longestPalindrome(string s) {
+        string s1=oddLengthPalindrome(s);
+        string s2=evenLengthPalindrome(s);
+        return s1.size() > s2.size() ? s1 : s2;
     }
 };
