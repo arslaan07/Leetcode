f634bfd389dfdd0260448e32f30dec4fb04d4bae
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-11-13 17:56:19 +0530

    Time: 54 ms (22.80%), Space: 12.7 MB (60.63%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
+    bool isVowel(char c) {
+        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A'
+              || c=='E' || c=='I' || c=='O' || c=='U'){
+            return true;
+        }
+        return false;
+    }
     string sortVowels(string s) {
         string vowel = "AEIOUaeiou";
         map<char,int> mpp;                             
         for(int i=0; s[i]!='\0'; i++) {
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A'
-              || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
+            if(isVowel(s[i])){
             mpp[s[i]]++;
+            }
         }
         string ans = "";
         int i = 0;
         int j = 0;
         while (i != s.size()) {
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A'
-              || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U') {
+            if(isVowel(s[i])) {
                 if(mpp.find(vowel[j]) != mpp.end()) {
                     ans.push_back(vowel[j]);
                     mpp[vowel[j]]--;
