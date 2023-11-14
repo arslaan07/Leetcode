e84c69a48fdb7e50d9db80ce56db6425af219329
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-11-14 11:22:59 +0530

    Time: 957 ms (23.98%), Space: 14.2 MB (38.81%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        int n = s.size();
+        map<char,int> mpp1;
+        map<char,int> mpp2;
+        for(int i=0; i<n; i++) {
+            mpp1[s[i]]++;
+        }
+        for(int i=0; i<n; i++) {
+            mpp2[s[i]] = i;
+        }
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            if(mpp1[s[i]] > 1) {
+                set<char> st;
+                for(int j=i+1; j<=mpp2[s[i]]-1; j++) {
+                    st.insert(s[j]);
+                }
+                ans += st.size();
+                mpp1[s[i]] = 0;
+            }
+        }
+        return ans;
+    }
+};
