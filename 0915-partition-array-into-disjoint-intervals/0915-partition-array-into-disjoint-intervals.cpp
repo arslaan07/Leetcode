4aa2cd751fa0fa77e3de347ed1b022fb57b87b70
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-11-14 22:35:45 +0530

    Time: 138 ms (10.07%), Space: 102.7 MB (15.03%) - LeetHub

--- 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.cpp
+++ 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int partitionDisjoint(vector<int>& nums) {
-
-        int  n = nums.size();
-        vector<int> pre(n,-1);
-        vector<int> post(n,-1);
-        int currMax = nums[0] , currMin = nums[n-1];
-        for(int i=0;i<n;i++){
-            currMax = max(currMax,nums[i]);
-            currMin = min(currMin,nums[n-1-i]);
-            post[n-1-i] = currMin;
-            pre[i] = currMax;
+        int n = nums.size();
+        vector<int> prefix(n,-1);
+        prefix[0] = nums[0];
+        vector<int> suffix(n,-1);
+        suffix[n-1] = nums[n-1];
+        for(int i=1; i<n; i++){
+            prefix[i] = max(prefix[i-1], nums[i]);
+            suffix[n-i-1] = min(suffix[n-i], nums[n-i-1]);
         }
-        cout<<n;
-        int ans ;
-        for(int i=0;i<n-1;i++){
-            if(pre[i] <= post[i+1]){
-                ans = i+1;
-                break;
-            }
+        int ans;
+        for(int i=0; i<=n-2; i++) {
+           if(prefix[i] <= suffix[i+1]) {
+               ans = i+1;
+               break;
+           }
         }
         return ans;
     }
