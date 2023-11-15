cfd066ce8c11d88dbfd941e389001d715b7eaee4
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-11-15 19:23:16 +0530

    Time: 49 ms (56.08%), Space: 48.6 MB (92.97%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,31 +2,18 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1) {
-            return true;
-        }
-        // else if(nums[0] == 0) {
-        //     return false;
-        // }
-        int li = n-1;
-        vector<int> prev (n,0);
-            int j=0;
-            while(j < n) {
-            if(nums[0] == 0) break;
-                if(nums[j] == 0) {
-                    j = prev.back();;
-                    prev.pop_back();
-                    nums[j]--;
-                    continue;
-                }
-                if(nums[j] >= li-j) {
-                    return true;
-                }
-                else{
-                    prev.push_back(j);
-                    j += nums[j];
-                }
+        int goal = n-1;
+        int jump = 1;
+        for(int i=n-2; i>=0; i--) {
+            if(nums[i] >= jump) {
+                goal -= jump;
+                jump = 1;
             }
+            else{
+                jump++;
+            }
+        }
+        if(goal == 0) return true;
         return false;
     }
 };
