f576570e1f7ae193220c5c0b8b1ed92bc13b05e8
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-11-17 11:12:27 +0530

    Time: 376 ms (5.08%), Space: 128.6 MB (5.08%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -2,15 +2,30 @@ class Solution {
 public:
     int minPairSum(vector<int>& nums) {
         int n = nums.size();
+        map<int, int> mpp;
+        for(int i=0; i<n; i++) {
+            mpp[nums[i]]++;
+        }
+        auto it1 = mpp.begin();
+        auto it2 = --mpp.end();
         int maxSum = INT_MIN;
-        sort(nums.begin(), nums.end());
-        int i = 0;
-        int j = n - 1;
-        while(i < j) {
-            int sum = nums[i] + nums[j];
+        while(it1 -> second !=0 && it2 -> second != 0) {
+            int sum = it1 -> first + it2 -> first;
             maxSum = max(maxSum, sum);
-            i++;
-            j--;
+            if(it1 -> second > it2 -> second) {
+            it1 -> second = it1 -> second - it2 -> second;
+            it2 -> second = 0;
+            }
+            else if(it2 -> second > it1 -> second) {
+            it2 -> second = max(it1 -> second, it2 -> second) - min(it1 -> second, it2 -> second);
+            it1 -> second = 0;
+            }
+            else{
+            it1 -> second = 0;
+            it2 -> second = 0;
+            }
+            if(it1 -> second == 0) it1++;
+            if(it2 -> second == 0) it2--;
         }
         return maxSum;
     }
