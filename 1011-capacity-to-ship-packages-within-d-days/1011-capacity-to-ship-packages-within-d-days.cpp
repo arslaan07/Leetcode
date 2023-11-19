bf23a6501f702fc3c69017f1d462f919f225ec8f
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-11-19 13:41:13 +0530

    Time: 59 ms (57.12%), Space: 31.4 MB (77.27%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canBeShipped(vector<int>& weights, int days, int weight) {
+        int n = weights.size();
+        int day = 0;
+        int sumOfWeights = 0;
+        for(int i=0; i<n; i++) {
+            sumOfWeights += weights[i];
+            if(sumOfWeights > weight) {
+                day++;
+                sumOfWeights = weights[i];
+            }
+        }
+        day++;
+        if(day <= days) return true;
+        return false;
+        
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n = weights.size();
+        int start = INT_MIN;
+        for(int i=0; i<n; i++) {
+            start = max(start, weights[i]);
+        }
+        int end = INT_MAX;
+        int ans;
+        while(start <= end) {
+            int m = start + (end - start >> 1);
+            if(canBeShipped(weights, days, m)) {
+                ans = m;
+                end = m - 1;
+            }
+            else{
+                start = m + 1;
+            }
+        }
+        return ans;
+    }
+};
