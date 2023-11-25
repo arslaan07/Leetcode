f8a5a82ba5bbc59bed080ac75afad27b2d198558
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-11-25 20:03:56 +0530

    Time: 291 ms (5.95%), Space: 90.1 MB (14.59%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -3,18 +3,16 @@ public:
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
         int n = nums.size();
         vector<int> result;
-        vector<int> prefix(n+1, 0);
-        prefix[0] = 0;
+        int prefix = 0;
         vector<int> suffix(n+1, 0);
         suffix[n] = 0;
-        for(int i=1; i<=n; i++) {
-            prefix[i] = prefix[i-1] + nums[i-1];
-        }
+        
         for(int i=n-1; i>=0; i--) {
             suffix[i] = suffix[i+1] + nums[i];
         }
         for(int i=0; i<n; i++) {
-            int left = nums[i] * i - prefix[i];
+            int left = nums[i] * i - prefix;
+            prefix = prefix + nums[i];
             int right = suffix[i+1] - nums[i] * (n-1-i);
             cout<<left<<" "<<right;
             int absDiff = left + right;
