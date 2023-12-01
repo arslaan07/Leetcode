a0ac712858e9da84d5dd51460102398c24e11aed
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-12-01 13:40:19 +0530

    Time: 268 ms (57.20%), Space: 23.9 MB (65.83%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int n = s.size();
+        int totalBeauty = 0;
+        for(int i=0; i<n; i++) {
+            unordered_map<char,int> mpp;
+            for(int j=i; j<n; j++) {
+                mpp[s[j]]++;
+                int minfreq = INT_MAX;
+                int maxfreq = INT_MIN;
+                for(auto it : mpp) {
+                    minfreq = min(minfreq, it.second);
+                    maxfreq = max(maxfreq, it.second);
+                }
+                totalBeauty += maxfreq-minfreq;
+            }
+        }
+        return totalBeauty;
+    }
+};
