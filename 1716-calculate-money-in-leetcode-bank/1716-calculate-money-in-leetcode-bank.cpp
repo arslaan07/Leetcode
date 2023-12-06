82ed68547e5e7ed87c36245f0ad79ee496788a98
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-12-06 11:51:14 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (70.53%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int times = n/7;
-        int sum = 0;
-        int i;
-        for(i=1; i<=times; i++) {
-            int k = i+6;
-            sum += k*(k+1)/2 - i*(i-1)/2;
-        }
-        int remainingTimes = n - times * 7;
+        int weeks = n/7;
+        int money = 0;
+        int i = weeks + 1;
+        money = max(0, 28 * weeks + 7 * (weeks)*(weeks-1) / 2);
+        int remainingTimes = n - weeks * 7;
         for(int j=1; j<=remainingTimes; j++) {
-            sum += i;
+            money += i;
             i++;
         }
-        return sum;
+        return money;
     }
 };
