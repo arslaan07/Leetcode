599916ae99222d15bd76047aa82217b3c0a1bc08
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-12-11 12:45:45 +0530

    Time: 8 ms (78.76%), Space: 12.8 MB (72.53%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int findSpecialInteger(vector<int>& arr) {
-        int sz = arr.size();
-        vector<int> candidates = {arr[sz/4], arr[sz/2], arr[sz*3/4]};
-        for (auto cand : candidates) {
-            auto st = lower_bound(arr.begin(), arr.end(), cand);
-            auto ed = upper_bound(arr.begin(), arr.end(), cand);
-            if (4 * (distance(st, ed)) > sz)
-                return cand;
+        int n = arr.size();
+        vector<int> candidates = {arr[n/4], arr[n/2], arr[n*3/4]};
+        for(auto cand : candidates) {
+            auto lb = lower_bound(arr.begin(), arr.end(), cand);
+            auto ub = upper_bound(arr.begin(), arr.end(), cand);
+            if(4 * (distance(lb, ub)) > n) return cand;
+            
         }
         return -1;
     }
