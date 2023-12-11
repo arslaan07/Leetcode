343e681bbe51cc3f195d24c19d1dd98242831a11
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-12-11 12:46:34 +0530

    Time: 6 ms (92.41%), Space: 12.7 MB (92.26%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -4,9 +4,9 @@ public:
         int n = arr.size();
         vector<int> candidates = {arr[n/4], arr[n/2], arr[n*3/4]};
         for(auto cand : candidates) {
-            auto lb = lower_bound(arr.begin(), arr.end(), cand);
-            auto ub = upper_bound(arr.begin(), arr.end(), cand);
-            if(4 * distance(lb, ub) > n) return cand;
+            int lb = lower_bound(arr.begin(), arr.end(), cand) -arr.begin();
+            int ub = upper_bound(arr.begin(), arr.end(), cand) - arr.begin();
+            if(ub-lb > n/4) return cand;
             
         }
         return -1;
