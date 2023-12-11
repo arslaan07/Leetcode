8812b2cccef39ab830959b1dceee145ec7ea81a8
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-12-11 12:45:55 +0530

    Time: 10 ms (62.22%), Space: 12.7 MB (92.26%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -6,7 +6,7 @@ public:
         for(auto cand : candidates) {
             auto lb = lower_bound(arr.begin(), arr.end(), cand);
             auto ub = upper_bound(arr.begin(), arr.end(), cand);
-            if(4 * (distance(lb, ub)) > n) return cand;
+            if(4 * distance(lb, ub) > n) return cand;
             
         }
         return -1;
