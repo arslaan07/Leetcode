941ad32740aba66a6cb9463dfd6d48113b723c1f
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-12-13 15:10:26 +0530

    Time: 16 ms (68.95%), Space: 13.3 MB (46.58%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    bool checkSpecial(vector<vector<int>>& mat, int i, int j) {
-        int k=0;
-        int cnt=0;
-        while(k < mat[0].size()) {
-            if(mat[i][k] == 1) cnt++;
-            if(cnt > 1) return false;
-            k++;
-        }
-        k = 0;
-        cnt = 0;
-        while(k < mat.size()) {
-            if(mat[k][j] == 1) cnt++;
-            if(cnt > 1) return false;
-            k++;
-        }
-        return true;
-    }
     int numSpecial(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
-        int sp = 0;
+        vector<int> row(m, 0);
+        vector<int> col(n, 0);
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(mat[i][j] == 1) {
-                    int val = checkSpecial(mat, i, j);
-                    if(val == true) {
-                        sp++;
-                    }
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        int sp = 0;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(mat[i][j] == 1 && row[i] == 1 && col[j] == 1) {
+                    sp++;
                 }
             }
         }
