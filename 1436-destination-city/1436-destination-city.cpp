cc5fef6e368693baf4e55be40727be7ba6cbf654
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-12-15 16:25:12 +0530

    Time: 12 ms (35.41%), Space: 11.6 MB (30.98%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    bool check(vector<vector<string>>& paths, string s) {
-       for(int i=0; i<paths.size(); i++) {
-           if(s == paths[i][0]) {
-               return false;
-           }
-       }
-        return true;
+//     bool check(vector<vector<string>>& paths, string s) {
+//        for(int i=0; i<paths.size(); i++) {
+//            if(s == paths[i][0]) {
+//                return false;
+//            }
+//        }
+//         return true;
           
-    }
+//     }
     string destCity(vector<vector<string>>& paths) {
         int m = paths.size();
         unordered_map<string, int> mpp;
+        unordered_set<string> st;
         for(int i=0; i<m; i++) {
+            st.insert(paths[i][0]);
             for(int j=0; j<paths[i].size(); j++) {
                 mpp[paths[i][j]]++;
             }
         }
         for(auto it : mpp) {
-           if(it.second == 1) {
-               if(check(paths, it.first) ==true) {
-                   return it.first;
-               }
+           if(it.second == 1 && st.find(it.first) == st.end()) {
+                return it.first;
            }
         }
         return "";
