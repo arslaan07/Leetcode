7339cb4618065e742729067d34c439e6578bac73
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-12-15 16:29:21 +0530

    Time: 10 ms (62.92%), Space: 10.9 MB (85.89%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -2,18 +2,14 @@ class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
         int m = paths.size();
-        map<string, int> mpp;
         set<string> st;
         for(int i=0; i<m; i++) {
             st.insert(paths[i][0]);
-            for(int j=0; j<paths[i].size(); j++) {
-                mpp[paths[i][j]]++;
-            }
         }
-        for(auto it : mpp) {
-           if(it.second == 1 && st.find(it.first) == st.end()) {
-                return it.first;
-           }
+        for(int i=0; i<m; i++) {
+            if(st.find(paths[i][1]) == st.end()) {
+                return paths[i][1];
+            }
         }
         return "";
     }
