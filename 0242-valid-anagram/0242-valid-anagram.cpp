21a6292d5c67bc46df51359e620e7dcba44d7470
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-12-16 21:25:23 +0530

    Time: 4 ms (87.26%), Space: 7.8 MB (45.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) {
+            return false;
+        }
         vector<int> hashmap(26, 0);
         for(int i=0; i<s.size(); i++) {
             hashmap[s[i]-'a']++;
-        }
-        for(int i=0; i<t.size(); i++) {
             hashmap[t[i]-'a']--;
         }
         for(int i=0; i<26; i++) {
