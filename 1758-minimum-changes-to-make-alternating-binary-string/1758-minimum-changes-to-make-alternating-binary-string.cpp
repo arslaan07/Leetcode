57c08ea666afb56910bbbce1f4e0995c31e6d0e0
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-12-24 15:07:14 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (45.10%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -3,17 +3,36 @@ public:
     int minOperations(string s) {
         //formats: 0 1 0 1 0 1 0 1 ....
         //         1 0 1 0 1 0 1 0 ....
+        // int n = s.size();
+        // int startWithZero = 0;
+        // int startWithOne = 0;
+        // for(int i=0; i<n; i++) {
+        //     if(i%2 == 0) {
+        //         s[i] == '0' ? startWithZero++ : startWithOne++;
+        //     }
+        //     else {
+        //         s[i] == '1' ? startWithZero++ : startWithOne++;
+        //     }
+        // }
+        // return min(startWithZero, startWithOne);
+        
+        //Optimised approach
         int n = s.size();
         int startWithZero = 0;
-        int startWithOne = 0;
         for(int i=0; i<n; i++) {
             if(i%2 == 0) {
-                s[i] == '0' ? startWithZero++ : startWithOne++;
+                if(s[i] == '0') {
+                  startWithZero++;  
+                }
             }
             else {
-                s[i] == '1' ? startWithZero++ : startWithOne++;
+                if(s[i] == '1') {
+                  startWithZero++;  
+                }
             }
         }
+        int startWithOne = n - startWithZero;
         return min(startWithZero, startWithOne);
+        
     }
 };
