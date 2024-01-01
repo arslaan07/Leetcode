b474a2a97adb8ceea68e00ce34130db170f73599
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2024-01-01 15:48:44 +0530

    Time: 24 ms (46.16%), Space: 17.8 MB (36.11%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -5,7 +5,6 @@ public:
         sort(s.begin(), s.end());
         int mg = g.size();
         int ns = s.size();
-        if(ns == 0) return 0;
         int i = mg-1;
         int j = ns-1;
         int cnt = 0;
