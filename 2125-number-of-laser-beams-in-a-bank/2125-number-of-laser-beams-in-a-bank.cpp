eef9f9b74f0b335e3a756ee49476e4654f2c312c
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2024-01-03 20:37:34 +0530

    Time: 104 ms (24.97%), Space: 23.2 MB (96.49%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -3,18 +3,14 @@ public:
     int numberOfBeams(vector<string>& bank) {
         int n = bank.size();
         int prevSum = 0;
-        for(int i=0; i<bank[0].size(); i++) {
-            prevSum += bank[0][i] - '0';
-        }
         int ans = 0;
-        for(int i = 1; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             int currSum = 0;
             for(int j = 0; j < bank[i].size(); j++) {
                 currSum += bank[i][j] - '0';
             }
             if(prevSum * currSum) {
                 int mul = currSum * prevSum;
-                cout << mul << " ";
                 ans += mul;
             }
             if(currSum != 0)
