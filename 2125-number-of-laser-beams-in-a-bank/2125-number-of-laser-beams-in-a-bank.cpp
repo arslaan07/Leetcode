f72e483f445115a57a8006d1b976bc91305561a3
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2024-01-03 20:33:57 +0530

    Time: 105 ms (23.63%), Space: 23.3 MB (96.49%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
         int n = bank.size();
-        vector<int> temp;
-        for(int i = 0; i < n; i++) {
-            int sum = 0;
+        int prevSum = 0;
+        for(int i=0; i<bank[0].size(); i++) {
+            prevSum += bank[0][i] - '0';
+        }
+        int ans = 0;
+        for(int i = 1; i < n; i++) {
+            int currSum = 0;
             for(int j = 0; j < bank[i].size(); j++) {
-                sum += bank[i][j] - '0';
+                currSum += bank[i][j] - '0';
             }
-            if(sum != 0)
-                temp.push_back(sum);
+            if(prevSum * currSum) {
+                int mul = currSum * prevSum;
+                cout << mul << " ";
+                ans += mul;
+            }
+            if(currSum != 0)
+            prevSum = currSum;
         }
-
-        int mul = 1;
-        int add = 0;
-
-       
-        if(temp.size() >= 2)
-        for(int i = 0; i < temp.size() - 1; i++) {
-            mul = temp[i] * temp[i+1];
-            add += mul;
-        }
-
-        return add;
+        return ans;
     }
 };
