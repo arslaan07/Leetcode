8703f59efbe685fcbfbcb5fba43e8e0b72e95994
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2024-02-10 11:25:09 +0530

    Time: 5 ms (64.45%), Space: 7.7 MB (68.03%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,25 +1,31 @@
 class Solution {
+    int oddSubstr(string s, int n) {
+        int cnt = 0;
+        for(int i=0; i<n; i++) {
+            int j = 0;
+            while(i-j >= 0 && i+j <= n-1 && s[i-j] == s[i+j]) {
+                cnt++;
+                j++;
+            }
+        }
+        return cnt;
+    }
+    int evenSubstr(string s, int n) {
+        int cnt = 0;
+        for(double i=0.5; i<n; i++) {
+            double j = 0.5;
+            while(i-j >= 0 && i+j <= n-1 && s[i-j] == s[i+j]) {
+                cnt++;
+                j++;
+            }
+        }
+        return cnt;
+    }
 public:
-    int countSubstrings(string str) {
-    int n=str.length();
-    int count = 0;
-
-    for (int i = 0; i < n; i++) {
-        // Counting odd-length palindromes
-        int j=0;
-        while(i-j>=0 && i+j<=n-1 && str[i-j]==str[i+j]){
-            count++;
-            j++;
-        }
-    }
-    for (double i = 0.5; i < n; i++) {
-        // Counting even-length palindromes
-        double j=0.5;
-        while(i-j>=0 && i+j<=n-1 && str[int(i-j)]==str[int(i+j)]){
-            count++;
-            j++;
-        }
-    }
-    return count;
+    int countSubstrings(string s) {
+        int n = s.size();
+        int o = oddSubstr(s, n);
+        int e = evenSubstr(s, n);
+        return o+e;
     }
 };
