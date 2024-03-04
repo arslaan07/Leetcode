053ca1881304626f8603f037be87ab3c87fa252c
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2024-03-04 19:30:17 +0530

    Time: 3 ms (86.63%), Space: 13.2 MB (23.53%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.cpp
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    int k = 0;
-    int sc = 0;
-    int getPower(vector<int>& tokens, int power) {
-        int n = tokens.size();
-        power += tokens[n-k-1];
-        k++;
-        sc--;
-        return power;
-    }
     int bagOfTokensScore(vector<int>& tokens, int power) {
-        sort(tokens.begin(), tokens.end());
         int n = tokens.size();
+        int i = 0; 
+        int j = n-1;
+        int sc = 0;
         int maxSc = 0;
-        for(int i=0; i<n; i++) {
-            if(n-k-1 < i) break;
+        sort(tokens.begin(), tokens.end());
+        while(i <= j) {
             if(power >= tokens[i]) {
+                power -= tokens[i];
                 sc++;
                 maxSc = max(sc, maxSc);
-                power -= tokens[i];
+                i++;
             }
             else {
-                if(i == n-k-1 || sc <= 0) break;
-                power = getPower(tokens, power);
-                if(power >= tokens[i]) {
-                sc++;
-                maxSc = max(sc, maxSc);
-                power -= tokens[i];
-            }
+                if(sc >= 1) {
+                    power += tokens[j];
+                    sc--;
+                }
+                else {
+                    break;
+                }
+                j--;
             }
         }
-    return maxSc;
+        return maxSc;
     }
 };
