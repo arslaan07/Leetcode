1ea485f95a65f811cdbe251ae50d39ea2ea3e4c6
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2024-03-05 19:22:18 +0530

    Time: 26 ms (55.86%), Space: 13.9 MB (44.14%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int minimumLength(string s) {
         int n = s.size();
-        cout << n << endl;
         int l = 0;
         int r = n-1;
         int deleted = 0; 
@@ -12,13 +11,10 @@ public:
                 while(l+1 != r && s[l+1] == s[l]) {
                     l++;
                     deleted++;
-                    cout << deleted << " ";
-
                 }
                 while(r-1 != l && s[r-1] == s[r]) {
                     r--;
                     deleted++;
-                    cout << deleted << " ";
                 }
                 l++;
                 r--;
