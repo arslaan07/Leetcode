f21401eaada1fcbbbfb0caef5dea32f7b4ecb951
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2024-03-11 19:16:48 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (12.46%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -1,27 +1,9 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        int n = order.size();
-        int m = s.size();
-        string pre = "";
-        string suc = "";
-        set<char> st;
-        for(auto ch : order) {
-            st.insert(ch);
-        }
-        for(auto ch : s) {
-            if(st.find(ch) == st.end()) {
-                suc += ch;
-            }
-        }
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(order[i] == s[j]) {
-                    pre += s[j];
-                }
-            }
-            
-        }
-        return pre+suc;
+        sort(s.begin(), s.end(), [order](char c1, char c2) {
+            return order.find(c1) < order.find(c2);
+        });
+        return s;
     }
 };
