3d467863b73baafab3bf5f53c71557092a54b920
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2024-03-14 13:14:16 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (74.39%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        int sum = n*(n+1)/2;
-        int prefix = 0;
-        int suffix = 0;
-        for(int i=1; i<=n; i++) {
-            suffix = sum - prefix;
-            prefix = i + prefix;
-            if(prefix == suffix) return i;
-        }
-        return -1;
+        int sum = n*(n+1) >> 1;
+        int x = sqrt(sum);
+        return x*x == sum ? x : -1;    
     }
 };
