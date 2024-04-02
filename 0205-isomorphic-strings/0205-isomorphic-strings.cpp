c5cadb64af2ace70a2436c7771d7a59c1153c688
theLazyProgamR <108570502+arslaan07@users.noreply.github.com>
2024-04-02 11:07:50 +0530

    Time: 7 ms (48.84%), Space: 9.5 MB (17.52%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         int n = s.size();
-        unordered_map<char, char> rep;
-        unordered_map<char, bool> used;
+        unordered_map<char,char> rep;
+        unordered_map<char,bool> used;
         for(int i=0; i<n; i++) {
             if(rep.count(s[i])) {
-                if(rep[s[i]] != t[i])
+                if(rep[s[i]] != t[i]) {
                     return false;
+                }
             }
             else {
-                if(used[t[i]]) {
+                if(used[t[i]] == true) {
                     return false;
-                }    
+                }
             }
             rep[s[i]] = t[i];
             used[t[i]] = true;
