c8742834baa7c11ad91f0d12d2e36a1bf45ce358
theLazyProgamR <108570502+arslaan07@users.noreply.github.com>
2024-04-18 20:48:17 +0530

    Time: 6 ms (90.28%), Space: 20.8 MB (66.34%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -9,29 +9,24 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-unordered_map<TreeNode*, int> profitMap;
 class Solution {
 public:
-    int rob(TreeNode* root) {
-        if(profitMap.find(root) != profitMap.end()) {
-		return profitMap[root];
-	}
+    pair<int, int> robTopDownSpaceOptimized(TreeNode* root) {
 	// base case
 	if(root == NULL) {
-		return profitMap[root] = 0;
+		return {0,0};
 	}
-	// recursive case
+	// recusive case
+	auto[leftDontRob, leftRob] = robTopDownSpaceOptimized(root->left);
+	auto[rightDontRob, rightRob] = robTopDownSpaceOptimized(root->right);
 
-	// 1. rob at the root node
-	int robRoot = root->val;
-	if(root->left != NULL)
-		robRoot += rob(root->left->left) + rob(root->left->right); 
-	if(root->right!= NULL)
-		robRoot += rob(root->right->left) + rob(root->right->right); 
-
-	// 2. do not rob at root node
-	int doNotRobRoot = rob(root->left) + rob(root->right);
-
-	return profitMap[root] = max(robRoot, doNotRobRoot);
+	int x = max(leftDontRob, leftRob);
+	int y = max(rightDontRob, rightRob);
+	int z = leftDontRob + rightDontRob + root->val;
+	return {x+y, z};
+}
+    int rob(TreeNode* root) {
+       pair<int, int> p = robTopDownSpaceOptimized(root);
+        return max(p.first, p.second);
     }
 };
