d8b3d31fd307649720fb7f87996c284aedc77bc1
theLazyProgamR <108570502+arslaan07@users.noreply.github.com>
2024-04-20 18:39:39 +0530

    Time: 159 ms (37.46%), Space: 89.5 MB (36.73%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    int maximum = INT_MIN;
-    int lI = 0;
-    int lJ = 0;
-    void endOfFarm(vector<vector<int>>& land, int i, int j, int m, int n) {
+    void endOfFarm(vector<vector<int>>& land, int i, int j, int m, int n, int sum, pair<int, int>& p) {
     // base case
         if(i == -1 || i == m || j == -1 || j == n || land[i][j] == 0 || land[i][j] == -1) {
            return;
         }
-        if(i+j > maximum) {
-            maximum = i+j;
-            lI = i;
-            lJ = j;
-        }
         
     // recursive case
+        if(i+j > sum) {
+            sum = i+j;
+            p = {i,j};
+        }
         land[i][j] = -1;
-        endOfFarm(land, i-1, j, m, n);
-        endOfFarm(land, i+1, j, m, n);
-        endOfFarm(land, i, j-1, m, n);
-        endOfFarm(land, i, j+1, m, n);
+        endOfFarm(land, i-1, j, m, n, sum, p);
+        endOfFarm(land, i+1, j, m, n, sum, p);
+        endOfFarm(land, i, j-1, m, n, sum, p);
+        endOfFarm(land, i, j+1, m, n, sum, p);
         
         return;
     }
@@ -31,15 +27,13 @@ public:
             for(int j=0; j<n; j++) {
                 vector<int> ans;
                 if(land[i][j] == 1) {
-                    endOfFarm(land, i, j, m, n);
+                    pair<int, int> p = {i,j};
+                    endOfFarm(land, i, j, m, n, i+j, p);
                     ans.push_back(i);
                     ans.push_back(j);
-                    ans.push_back(lI);
-                    ans.push_back(lJ);
+                    ans.push_back(p.first);
+                    ans.push_back(p.second);
                     ds.push_back(ans);
-                    maximum = INT_MIN;
-                    lI = 0;
-                    lJ = 0;
                 }
             }
         }
