8c26d99b9c0dd4bead0e78fc905fc687f3c62f85
theLazyProgramR <108570502+arslaan07@users.noreply.github.com>
2024-04-24 19:36:33 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (5.38%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -10,15 +10,29 @@ public:
         return dp[n] = topDownTrribonacci(n-1, dp) + topDownTrribonacci(n-2, dp) + topDownTrribonacci(n-3, dp);
         
     }
+    int bottomUpTrribonacci(int n) {
+        vector<int> dp(n+1); 
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        
+        for(int i=3; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        return dp[n];
+    }
     int tribonacci(int n) {
 //         // base case
-//         if(n <= 1) return n;
-//         if(n==2) return 1;
+        if(n <= 1) return n;
+        if(n==2) return 1;
         
 //         // recursive case
 //         return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
         
-        vector<int> dp(n+1, -1);
-        return topDownTrribonacci(n, dp);
+        // vector<int> dp(n+1, -1);
+        // return topDownTrribonacci(n, dp);
+        
+        return bottomUpTrribonacci(n);
     }
 };
